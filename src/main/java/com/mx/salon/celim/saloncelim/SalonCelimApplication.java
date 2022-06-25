e167e643e03fffc31c0121c687da34e1aecef989
eduar2ch <eduar2ch@gmail.com>
2022-06-25 14:22:09 -0500

    primer commint del proyecto

---
+++ src/main/java/com/mx/salon/celim/saloncelim/SalonCelimApplication.java
@@ -0,0 +1,13 @@
+package com.mx.salon.celim.saloncelim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonCelimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonCelimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/salon/celim/saloncelim/SalonCelimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.salon.celim.saloncelim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonCelimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
